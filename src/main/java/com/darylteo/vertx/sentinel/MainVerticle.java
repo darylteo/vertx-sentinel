b4dad085e11eb8312cb12101b898288a6534883a
darylteo <i.am@darylteo.com>
2013-09-18 18:33:26 +1000

    Add deployments

---
+++ src/main/java/com/darylteo/vertx/sentinel/MainVerticle.java
@@ -0,0 +1,15 @@
+package com.darylteo.vertx.sentinel;
+
+import org.vertx.java.platform.Verticle;
+
+public class MainVerticle extends Verticle {
+  @Override
+  public void start() {
+    super.start();
+  }
+
+  @Override
+  public void stop() {
+    super.stop();
+  }
+}
